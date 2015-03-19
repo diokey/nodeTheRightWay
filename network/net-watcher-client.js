0411af0d579fa163332e843410959fd362f439f9
Olivier Dusabimana <diokeyolivier@gmail.com>
2015-03-18 21:41:36 -0400

    Updated watcher-client to use a custom module

--- network/net-watcher-client.js
+++ network/net-watcher-client.js
@@ -2,15 +2,19 @@
 
 const 
 net = require('net'),
-client = net.connect({port : 5432});
+client = net.connect({port : 5432}),
+ldj = require('./ldj.js'),
+ldjClient = ldj.connect(client)
+;
 
-client.on('data', function(data) {
-  let message = JSON.parse(data);
+ldjClient.on('message', function(message) {
 
   if(message.type ==='watching') {
     console.log('Now watching file ' + message.file);
   } else if (message.type === 'changed') {
     let time = new Date(message.timestamp);
     console.log('File '+message.file+ ' changed at '+time);
+  } else {
+    throw Error('Unrecognized message type');
   }
 });
