e214d98ddd2c7bc0dc37af056a73e2834b46e96c
Olivier Dusabimana <diokeyolivier@gmail.com>
2015-03-20 20:49:59 -0400

    Added zmq requester

---
+++ messaging/zmq-filer-req.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const 
+  zmq = require('zmq'),
+  filename = process.argv[2],
+
+  //create a request end point
+  requester = zmq.socket('req');
+
+  //parse incoming response
+  requester.on("message", function(data) {
+    let response = JSON.parse(data);
+    console.log("Received response:", response);
+  });
+  requester.connect("tcp://localhost:5433");
+    // send request for content
+  
+    for (let i=1; i<=3; i++) {
+      console.log('Sending request for ' + filename);
+      requester.send(JSON.stringify({
+        path: filename
+      }));
+    }
