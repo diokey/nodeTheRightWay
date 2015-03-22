cc12bdc35d08eefe867c8f38d087fb20fd00cc2a
Olivier Dusabimana <diokeyolivier@gmail.com>
2015-03-21 21:42:15 -0400

    Fixed a minor bug

--- messaging/zmq-filer-rep-cluster.js
+++ messaging/zmq-filer-rep-cluster.js
@@ -7,23 +7,21 @@ const
 if (cluster.isMaster) {
   //master process - create ROUTER and DEALER sockets, bind endpoints
   let 
-    router = zmq.socket('router').bind('tpc://127.0.0.1:5433'),
+    router = zmq.socket('router').bind('tcp://127.0.0.1:5433'),
     dealer = zmq.socket('dealer').bind('ipc://filer-dealer.ipc');
 
   //forward messages between router and dealer
   router.on('message', function () {
-    console.log('router received message');
     let frames = Array.prototype.slice.call(arguments); 
     dealer.send(frames);
   });
 
   dealer.on('message', function () {
-    console.log('dealer received message');
-    let frames = Array.prototype.slice.call(null, arguments);
+    let frames = Array.prototype.slice.call(arguments);
     router.send(frames);
   });
 
-  //listen for worker to come online
+  //listen for workers to come online
   cluster.on('online', function(worker) {
     console.log('Worker '+worker.process.pid+' is online');
   });
@@ -37,7 +35,6 @@ if (cluster.isMaster) {
   let responder = zmq.socket('rep').connect('ipc://filer-dealer.ipc');
  
   responder.on('message', function(data) {
-    console.log('message received');
     //parse incoming message
     let request = JSON.parse(data);
 
