5f9940c61abeaebb9bcbefa4c44d38a677cd4a37
Olivier Dusabimana <diokeyolivier@gmail.com>
2015-03-24 00:24:45 -0400

    Fixed a failing test

--- database/lib/rdf-parser.js
+++ database/lib/rdf-parser.js
@@ -9,12 +9,17 @@ module.exports = function(filename, callback) {
       $ = cheerio.load(data.toString()),
       collect = function(index, elem) {
         return $(elem).text();
+      },
+      parseSubjects = function () {
+        return $('[rdf\\:resource$="/LCSH"]').map(function(index, el) {
+          return $(el).parent().find('rdf\\:value').text();
+        }).get();
       };
       callback(null, {
         _id: $('pgterms\\:ebook').attr('rdf:about').replace('ebooks/', ''),
         title: $('dcterms\\:title').text(),
-        authors: $('pgterms\\:agent pgterms\\:name').map(collect),
-        subjects: $('[rdf\\:resource="*/LCSH"] ~ rdf\\:value').map(collect)
+        authors: $('pgterms\\:agent pgterms\\:name').map(collect).get(),
+        subjects: parseSubjects()
       });
   });
 };
