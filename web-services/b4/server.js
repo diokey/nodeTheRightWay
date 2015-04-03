a1c725cf721f62fa1e133ca36050fc33636e7c89
Olivier Dusabimana <diokeyolivier@gmail.com>
2015-04-02 21:30:12 -0400

    Include bundle api

--- web-services/b4/server.js
+++ web-services/b4/server.js
@@ -18,7 +18,7 @@ app.use(morgan('combined'));
 
 //require('./lib/book-search.js')(config, app);
 require('./lib/field-search.js')(config, app);
-//require('./lib/bundle.js')(config, app);
+require('./lib/bundle.js')(config, app);
 
 app.listen(3000, function () {
   console.log('listening to port 3000');
