91ccd44f8beafe6ce5703ec59ac0deedc4583812
Olivier Dusabimana <diokeyolivier@gmail.com>
2015-04-04 18:08:08 -0400

    Support PUT in the API

--- web-services/b4/lib/bundle.js
+++ web-services/b4/lib/bundle.js
@@ -42,10 +42,42 @@ module.exports = function(config, app) {
     .then(function (args) {
       let couchRes = args[0],
         body = JSON.parse(args[1]);
-      console.log(body);
       res.status(couchRes.statusCode).json(body);
     }, function (err) {
       res.status(502).json({error : 'bad_gateway', reason : err.code});
     }).done();
+  });
+
+  app.put('/api/bundle/:id/name/:name', function (req, res) {
+    let params = {
+      method : 'GET',
+      url : config.b4db + '/' + req.params.id,
+      auth : config.credentials,
+    };
+
+    Q.nfcall(request, params)
+    .then(function (args) {
+      let couchRes = args[0],
+      bundle = args[1];
+
+      if (couchRes.statusCode !== 200) {
+        return [couchRes, bundle];
+      }
+
+      bundle.name = req.params.name;
+
+      params.method = 'PUT';
+      params.json = bundle;
+      return Q.nfcall(request, params) 
+      .then(function (args) {
+        let couchRes = args[0],
+          body = args[1];
+      })
+      .catch(function(err) {
+        res.json(502, {error : "bad_gateway", reason : err.code});
+      })
+      .done();
+    });
+    
   });
 };
