c06d4b0754774b1eb16d3737d4cc8ccf348a5b2e
Olivier Dusabimana <diokeyolivier@gmail.com>
2015-04-10 23:18:04 -0400

    Authentification
    Implemented authentification with google-passport and storing
    session data with redis

--- webapp/b4/server.js
+++ webapp/b4/server.js
@@ -2,17 +2,51 @@
 
 const 
   express = require('express'),
+  cookieParser = require('cookie-parser'),
+  session = require('express-session'),
   app = express(),
-  redis = require('redis'),
-  redisConnect = require('redis-connect');
+  redisClient = require('redis').createClient(),
+  RedisStore = require('connect-redis')(session),
+  passport = require('passport'),
+  GoogleStrategy = require('passport-google').Strategy
+  ;
 
-app.use(express.cookieParser());
-app.use(express.session({
-  secret : 'mysecredhash',
-  store : new redisConnect({
-    client : redis
-  })
+  app.use(cookieParser());
+  app.use(session({
+    secret : 'mysecredhash',
+    resave : false,
+    saveUninitialized : true,
+    store : new RedisStore({
+      client : redisClient
+    })
+  }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function (user, done) {
+  done(null, user.identifier);
+});
+
+passport.deserializeUser(function(id, done) {
+  done(null, {identifier : id});
+});
+
+passport.use(new GoogleStrategy({
+  returnURL : 'http://localhost:3000/auth/google/return',
+  realm : 'http://localhost:3000/'
+},
+function (identifier, profile, done) {
+  profile.identifier = identifier;
+  return done(null, profile);
 }));
+
+
+app.get('/auth/google/:return?', passport.authenticate('google', {successRedirect : '/'}));
+app.get('/auth/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
+});
 
 app.use(express.static(__dirname + '/static'));
 app.use(express.static(__dirname +'/bower_components'));
